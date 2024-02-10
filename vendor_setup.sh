68215e09323f5aa0a777b8ce8a9dbde7bc3ced97
Massatrio16 <146039181+Massatrio16@users.noreply.github.com>
2024-02-11 05:21:23 +0700

    Create vendor_setup.sh

---
+++ vendor_setup.sh
@@ -0,0 +1,5 @@
+export OF_MAINTAINER=Massatrio16
+export OF_FL_PATH1="/sys/devices/virtual/torch/torch" 
+export OF_FL_PATH2="/sys/devices/virtual/sub_torch/sub_torch/"
+export OF_FL_PATH3="/sys/devices/virtual/flashlight_core/flashlight/"
+export OF_FLASHLIGHT_ENABLE=1
